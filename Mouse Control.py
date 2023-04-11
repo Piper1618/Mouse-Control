0402dbce240f887cfc3618cfe647002d62f8bc79
Piper <piper16180@gmail.com>
2023-04-11 00:29:18 -0700

    Initial commit

---
+++ Mouse Control.py
@@ -0,0 +1,136 @@
+#####
+# A Python script for OBS that allows a source to be controlled
+# using the mouse. One source in one scene can be controlled. If
+# you want to have a source move in multiple scenes, you can
+# create a blank scene, add the source you want to move, and
+# then add scene sources to other scenes where it should be
+# visible.
+#
+# Dependent on pynput: https://pypi.org/project/pynput/
+# pip install pynput
+#
+# Tested on Python 3.6 and OBS version 28.0.2
+#####
+
+import obspython as obs
+from pynput import mouse as Mouse
+
+scene_item      = None
+scene_name      = ""
+source_name     = ""
+scale           = 1
+offset_x        = 0
+offset_y        = 0
+listener        = None
+pos             = obs.vec2()
+
+
+# ------------------------------------------------------------
+# -- My functions
+# ------------------------------------------------------------
+
+def initialize():
+	global scene_item
+	global scene_name
+	global source_name
+
+	# Remove the callback in case this is from a repeating timer
+	obs.remove_current_callback()
+
+	# Get the list of scenes
+	scene_list = obs.obs_frontend_get_scenes()
+
+	# Search for the scene and source we want
+	for scene_source in scene_list: # scene_source is a source, not a scene. See note at end of function
+		name = obs.obs_source_get_name(scene_source)
+		if name == scene_name:
+			# Found!
+			scene_item = obs.obs_scene_find_source(obs.obs_scene_from_source(scene_source), source_name)
+			obs.source_list_release(scene_list)
+			return
+
+	# We didn't find what we were looking for, so try again later
+	obs.timer_add(initialize, 2000)
+
+	# Release the list of scenes from memory
+	obs.source_list_release(scene_list)
+
+	# Note on scenes and sources:
+	# The obs_frontend_get_scenes funciton does not actually return a list of
+	# scenes. Everything in OBS is a source and the list contains the source
+	# for each scene. For functions that need the actual scene object,
+	# obs_scene_from_source is used to extract ¿cast? the scene from its source.
+
+def on_mouse_move(x, y):
+	global scene_item
+	global scale
+	global offset_x
+	global offset_y
+	global pos
+
+	if scene_item is not None and obs.obs_sceneitem_visible(scene_item):
+		obs.vec2_set(pos, (x + offset_x) * scale, (y + offset_y) * scale)
+		obs.obs_sceneitem_set_pos(scene_item, pos)
+
+# ------------------------------------------------------------
+# -- Built-in functions
+# ------------------------------------------------------------
+
+def script_description():
+	return "Make a source move by copying mouse movement."
+
+
+def script_load(settings):
+	global listener
+
+	#print("loading")
+
+	initialize()
+
+	if listener is None:
+		listener = Mouse.Listener(
+				on_move = on_mouse_move
+		)
+		listener.start()
+		#listener.stop()
+
+
+def script_update(settings):
+	global scene_name
+	global source_name
+	global scale
+	global offset_x
+	global offset_y
+
+	#print("updating")
+
+	scale       = obs.obs_data_get_double(settings, "scale")
+	offset_x    = obs.obs_data_get_double(settings, "offset_x")
+	offset_y    = obs.obs_data_get_double(settings, "offset_y")
+	scene_name = obs.obs_data_get_string(settings, "scene")
+	source_name = obs.obs_data_get_string(settings, "source")
+	
+	initialize()
+
+
+def script_unload():
+	global listener
+	listener.stop()
+
+
+def script_defaults(settings):
+	obs.obs_data_set_default_double(settings, "scale", 1)
+	obs.obs_data_set_default_double(settings, "offset_x", 0)
+	obs.obs_data_set_default_double(settings, "offset_y", 0)
+
+
+def script_properties():
+	props = obs.obs_properties_create()
+
+	obs.obs_properties_add_text(props, "scene", "Scene Name", obs.OBS_TEXT_DEFAULT)
+	obs.obs_properties_add_text(props, "source", "Source Name", obs.OBS_TEXT_DEFAULT)
+	obs.obs_properties_add_float(props, "offset_x", "X offset", -1000, 1000, 1)
+	obs.obs_properties_add_float(props, "offset_y", "Y offset", -1000, 1000, 1)
+	obs.obs_properties_add_float(props, "scale", "Motion Scale", 0.01, 10, 0.2)
+
+	return props
---
+++ README.md
@@ -0,0 +1,21 @@
+# Mouse Control
+
+This script adds functionality to [OBS](https://obsproject.com/). It allows the position of a single source to be moved using the mouse. The chosen source will track mouse motion even if OBS is not in focus. I use this to overlay and control a pointer image over games that hide the mouse cursor. I have a global hotkey bound to show the overlaid pointer when there's something I want to point out.
+
+Only one source can be controlled. If you want to have identical sources move in multiple scenes, create a blank scene, add the source you want to move, and then add scene sources to other scenes where it should be visible. The source will move even if its scene is not the selected one. To truly control multiple sources, you'd need to create a copy of the script with a different name and import it separately.
+
+The only file that's needed is "Mouse Control.py". Once the file is on your local drive, the script can be imported from inside OBS by navigating to Tools -> Scripts. Because this is a Python script, the path in the Python Settings tab must be filled out.
+
+This was tested on Python 3.6 and OBS version 28.0.2.
+
+You'll also need to install pynput, which is used to read mouse positions: https://pypi.org/project/pynput/
+
+# Settings
+
+Scene Name: Enter the name of the scene where the source can be found.
+
+Source Name: Enter the name of the source to be controlled.
+
+Offsets: The mouse's position is offset by this amount. Negative values effectively move the origin point of the source being controlled. If you want to use the mouse's position on a monitor other than the main monitor, you'll also need to subtract the relevant monitor's position.
+
+Scale: Mouse movements will be multiplied by this scale before being applied to the source. This is applied after the offsets are added. For example, if your monitor is 1080p and your OBS canvas is 720p, you'll need to scale down the motion to 0.67.
